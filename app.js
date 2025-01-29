ca4eb279157f8c2e7053134a01fbae66dfd85ca7
Oluwole Olusegun Joshua <oluwoleolusegun14@gmail.com>
2025-01-29 21:48:40 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+const getInfo = (req,res) => {
+  const currentDateTime = new Date().toISOString()
+
+  res.status(200).json({
+    email: process.env.EMAIL,
+    current_dateTime: currentDateTime ,
+    github_url : process.env.GITHUB_URL
+  })
+}
+
+app.get("/", getInfo)
+
+module.exports = app
